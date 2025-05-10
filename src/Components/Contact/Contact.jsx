9301f089ee0e501a0147081998bee12d7bfe0f14
Joshua Clinton <joshuaclinton790@gmail.com>
2025-05-10 11:28:27 +0200

    Update portfolio section

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -11,16 +11,16 @@ function Contact() {
 
             <main class="form-container">
                 <h2 id='topic3'>Contact</h2>
-                <form name='contact' method="POST" data-netlify= "true" onSubmit="submit">
+                <form name="contact" method="POST" data-netlify="true" >
                     <input type="hidden" name='form-names' value='contact' />
                     <label for="name">Name:</label>
                     <input onChange={(e) => setText(e.target.value)} type="text" id="name" name="name" required/>
 
-                    <label htmlFor='email' for="email">Email:</label>
+                    <label for="email">Email:</label>
                     <input onChange={(e) => setText(e.target.value)}  type="email" id="email" name="email" required/>
 
                     <label for="message">Message:</label>
-                    <textarea id="message" name="message" rows="6" required ></textarea>
+                    <textarea id="message" name="message" ></textarea>
 
                    {check == true && text != ""? <label for="submit">Thank you submit for email</label>: <button onClick={() => setCheck(true)} type="submit">Send Message</button>} 
                 </form>
