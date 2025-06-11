f2dde508023efc6fa4c1fd1f53da8c829b92d374
Joshua Clinton <joshuaclinton790@gmail.com>
2025-06-11 11:22:34 +0200

    update

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -11,18 +11,23 @@ function Contact() {
 
             <main class="form-container">
                 <h1 id='topic12'>Contact</h1>
-                <form name="contact" method="POST" netlify netlify-honeypot="bot-field" >
+                <form name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field" >
                     <input type="hidden" name="form-name" value="contact" />
-                    <label for="name">Name:</label>
-                    <input onChange={(e) => setText(e.target.value) } type="text" id="name" name="name" required />
+                    <p hidden>
+                        <label>
+                            Don’t fill this out if you’re human: <input name="bot-field" />
+                        </label>
+                    </p>
+                    <label htmlFor="name">Name:</label>
+                    <input onChange={(e) => setText(e.target.value)} type="text" id="name" name="name" required />
 
-                    <label for="email">Email:</label>
-                    <input onChange={(e) => setText(e.target.value) } type="email" id="email" name="email" required />
+                    <label htmlFor="email">Email:</label>
+                    <input onChange={(e) => setText(e.target.value)} type="email" id="email" name="email" required />
 
-                    <label for="message">Message:</label>
+                    <label htmlFor="message">Message:</label>
                     <textarea id="message" name="message" ></textarea>
 
-                    {check == true && text != "" ? <label for="submit">I fix in email not work to me and please contact is joshuaclinton790@gmail.com</label> : <button onClick={() => setCheck(true)} type="submit">Send Message</button>}
+                    {check == true && text != "" ? (<label htmlFor="submit">I fix in email not work to me and please contact is joshuaclinton790@gmail.com</label>) : (<button onClick={() => setCheck(true)} type="submit">Send Message</button>)}
                 </form>
             </main>
 
