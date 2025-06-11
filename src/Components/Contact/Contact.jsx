242343293a8934b7e8404bcfadac5c702076cb19
Joshua Clinton <joshuaclinton790@gmail.com>
2025-06-11 14:27:35 +0200

    update

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -5,59 +5,26 @@ function Contact() {
 
 
     const [check, setCheck] = useState(false)
-    const [formData, setFormData] = useState({
-        name: "",
-        email: "",
-        message: "",
-    });
+    const [text, setText] = useState('')
 
-    const handleChange = (e) => {
-        setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        if (!formData.email) {
-            setCheck(true);
-            return;
-        }
-        e.target.submit();
-    };
     return (
         <>
 
-            <form
-                name="contact"
-                method="POST"
-                data-netlify="true"
-                data-netlify-honeypot="bot-field"
-                hidden
-            >
-                <input type="text" name="bot-field" />
-                <input type="text" name="name" />
-                <input type="email" name="email" />
-                <textarea name="message"></textarea>
-            </form>
-
             <main className="form-container">
                 <h1 id='topic12'>Contact</h1>
-                <form name="contact" method="POST" data-netlify="true" onSubmit={handleSubmit} >
+                <form name="contact" netlify >
                     <input type="hidden" name="form-name" value="contact" />
-                    <input type="hidden" name="form-name" value="contact" />
-                    <input type="hidden" name="bot-field" />
 
                     <label htmlFor="name">Name:</label>
-                    <input value={formData.name} onChange={handleChange} type="text" id="name" name="name" required />
+                    <input onChange={(e) => setText(e.target.value)} type="text" id="name" name="name" required />
 
                     <label htmlFor="email">Email:</label>
-                    <input value={formData.email} onChange={handleChange} type="email" id="email" name="email" required />
+                    <input onChange={(e) => setText(e.target.value)} type="email" id="email" name="email" required />
 
                     <label htmlFor="message">Message:</label>
-                    <textarea id="message" name="message" value={formData.message}
-            onChange={handleChange}></textarea>
+                    <textarea id="message" name="message" ></textarea>
 
-                    {check && !formData.email ? (<label htmlFor="submit">I fix in email not work to me and please contact is joshuaclinton790@gmail.com</label>) : (<button onClick={() => setCheck(true)} type="submit">Send Message</button>)}
+                    {check == true && text != "" ? (<label htmlFor="submit">I fix in email not work to me and please contact is joshuaclinton790@gmail.com</label>) : (<button onClick={() => setCheck(true)} type="submit">Send Message</button>)}
                 </form>
             </main>
 
