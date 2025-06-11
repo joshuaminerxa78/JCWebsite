a47b54037a0e13992cac20fdcdac926b179dd20d
Joshua Clinton <joshuaclinton790@gmail.com>
2025-06-11 15:14:39 +0200

    update

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -9,11 +9,11 @@ function Contact() {
 
     return (
         <>
-
+                <div>
             <main className="form-container">
                 <h1 id='topic12'>Contact</h1>
-                <form name="contact" data-netlify="true" >
-                    <input type="hidden" name="form-name" value="name_of_my_form" />
+                <form name="contact" method="POST" data-netlify="true" onSubmit="submit">
+                    <input type="hidden" name="form-name" value="contact" />
 
                     <label htmlFor="name">Name:</label>
                     <input onChange={(e) => setText(e.target.value)} type="text" id="name" name="name" required />
@@ -27,7 +27,7 @@ function Contact() {
                     {check == true && text != "" ? (<label htmlFor="submit">I fix in email not work to me and please contact is joshuaclinton790@gmail.com</label>) : (<button onClick={() => setCheck(true)} type="submit">Send Message</button>)}
                 </form>
             </main>
-
+            </div>
         </>
     )
 }
