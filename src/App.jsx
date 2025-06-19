022f5c32b8f123d3ab55bda292a365b841f6d7fb
Joshua Clinton <joshuaclinton790@gmail.com>
2025-06-19 17:10:49 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ import Footer from "./Components/Menu/footer";
 
 function App() {
   return (
+    // I make design used figma and there link: https://www.figma.com/proto/bcrqkpqHCZKDDnEOjhCZja/Photography-Portfolio-%E2%80%94-Webflow-Landing-Page-Design--Community-?node-id=1-1263&t=jtX2WoGBoLKqwuEb-1
+    // I did add Github but i add link to netlify add project and work show website and there link: https://joshuaclintonportfolio.netlify.app/
+    //I make add Github as step with powershell: git status, git add . , git commit -m "update", git push -u origin main. when update work saving to github and link to netlify update it.
     <BrowserRouter>
       <>
         <head><link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,opsz,wght@0,18..144,300..900;1,18..144,300..900&display=swap" rel="stylesheet"></link></head>
